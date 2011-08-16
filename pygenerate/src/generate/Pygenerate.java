896a8113f622123d5b11a3a93bc34ce3da9d2551
Martin Prout <>
2011-08-16 19:37:44 +0100

    Commit from NetBeans

---
+++ pygenerate/src/generate/Pygenerate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generate;
+
+/**
+ *
+ * @author sid
+ */
+public class Pygenerate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
